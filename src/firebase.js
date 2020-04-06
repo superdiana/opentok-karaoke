4b353fb32fa657f7a79889b84cbb74d65aea13ca
Kelly J Andrews <kelly@kellyjandrews.com>
2020-04-06 17:20:07 -0400

    move firebase config to use env file

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,16 @@
 //import firebase from 'firebase'
 import * as firebase from 'firebase/app'
 import 'firebase/auth'
-import config from './config'
 
-firebase.initializeApp(config.FIREBASE_CONFIG)
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+})
 
 export default firebase
