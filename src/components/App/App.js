0116b2d9a86ca251a7d1d5d1f292052c9e2b39b6
Kelly J Andrews <kelly@kellyjandrews.com>
2020-04-07 01:13:22 -0400

    block out initial content

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,94 +1,22 @@
-import React, {Component} from 'react';
+import React from 'react';
+import { Box, Flex, Heading } from '@chakra-ui/core';
+import Header from '../Header';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ExitToApp from '@material-ui/icons/ExitToApp'
-import Typography from '@material-ui/core/Typography';
+function App() {
 
-import Login from '../Login'
-import RoomManager from '../RoomManager'
-import Karaoke from '../Karaoke'
+  return (
+    <Flex direction="column" h="100vh">
+      <Box bg="gray.800" w="100vw" color="white">
+        <Header />
+      </Box>
+      <Flex direction="row" bg="gray.200" w="100vw" color="white" flex="1">
+        <Box bg="green.900" flexBasis="70vw" color="white"><Heading>video</Heading></Box>
+        <Box bg="red.900" color="white" flex="1"><Heading>Playlist</Heading></Box>
+      </Flex>
+      <Box bg="gray.900" w="100vw" h="20vh" color="white"><Heading>Queue</Heading></Box>
+    </Flex>
+  )
+};
 
-const classes = {
-  title: {
-    flexGrow: 1
-  },
-  toolbar: {
-    top: 0,
-    position: 'absolute'
-  },
-  session: {
-    color: '#fff',
-    fontSize: '15px',
-    verticalAlign: 'middle'
-  }
-}
-
-class  App extends Component {
-
-  constructor(props){
-    super(props)
-    this.state = {
-      username: sessionStorage.getItem('user'),
-      preauth: ((sessionStorage.getItem('user')!==null && sessionStorage.getItem('user')!==undefined)?true:false),
-      roomName:null,
-      roomSelected: false
-    }
-  }
-
-  handleInputChange(event) {
-    //console.log(event.target.name+" : "+event.target.value)                                                                                                                                                                                                            
-    this.setState({                                                                                                                                                                                                                             
-     [event.target.name]:event.target.value                                                                                                                                                                                               
-    })
-  }
-
-  handleCloseForm(event) {
-
-    //if(event.target.name && event.target.name === 'closeform')
-    this.setState({preauth: true})
-
-  }
-
-  handleRoomSelection(room) {
-    this.setState({roomName: room})
-  }
-
-  logOut(){
-    //this is the logOut method.
-  }
-
-  render(){
-    if(!this.state.preauth){
-      return (
-        <div className="App">
-          <header className="App-header">
-            <Login show={true} handleInputChange={this.handleInputChange.bind(this)} handleCloseForm={this.handleCloseForm.bind(this)}/>
-          </header>
-        </div>
-      )
-    } else {
-      return (
-        <div className="App">
-          <header className="App-header">
-            <AppBar position="static" style={classes.toolbar}>
-              <Toolbar>
-                <Typography variant="h6" style={classes.title}>
-                  Oka - OpenTok Karaoke
-                </Typography>
-                <div style={classes.session}>
-                  Connected as: <b>{JSON.parse(this.state.username).displayName}</b>
-                  <ListItemIcon onClick={() => this.logOut() } style={ {marginLeft: 10, cursor:'pointer', color:'#fff', verticalAlign: 'middle'} }><ExitToApp/></ListItemIcon>
-                </div>
-              </Toolbar>
-            </AppBar>
-            {this.state.roomName?<Karaoke roomName={this.state.roomName} username={this.state.username}/>:<RoomManager handleRoomSelection={this.handleRoomSelection.bind(this)}/>}
-          </header>
-        </div>
-      )
-    }
-  }
-}
 
 export default App;
