ec7fb9c74a30182aa706621ca7165cc8f7ab9fb0
Kelly J Andrews <kelly@kellyjandrews.com>
2020-04-07 00:07:22 -0400

    add chakra ui to index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { ThemeProvider, theme } from "@chakra-ui/core";
 import App from './components/App/App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const ThemedApp = () => <ThemeProvider theme={theme}> <App /> </ThemeProvider>;
+
+ReactDOM.render(<ThemedApp />,document.getElementById('root'));
